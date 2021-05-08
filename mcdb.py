98f675b9c0139589ccb978fbe3ed9dab61c5925c
WMCobb00 <Billycobb00@gmail.com>
2021-05-08 17:06:33 -0400

    Added several helper functions: update_members(), get_members_online(), and get_server_description(). Also made small updates to server()

--- mcdb.py
+++ mcdb.py
@@ -17,11 +17,11 @@ from datetime import datetime
 """ Client Vars """
 
 
-token = ''
+token = ''  # insert Discord bot token here
 cmd_prefix = '%'
 listening_to = cmd_prefix
 client = commands.Bot(command_prefix=cmd_prefix, help_command=None, case_insensitive=True)
-minecraft_server_ip = ''  # insert minecraft server ip here
+minecraft_server_ip = ''  # insert Minecraft server ip here
 
 
 """ Global Vars """
@@ -38,9 +38,70 @@ emojis = {
 
 """ Helper Functions """
 
-# used for reaction controls
+
+def get_server_description():
+	"""
+	Checks to see if and what the server's provided description is
+	:return:
+	"""
+	server = MinecraftServer.lookup(minecraft_server_ip)
+	status = server.status()
+
+	if 'text' in [key for key in status.raw['description']]:
+		server_desc = status.raw['description']['text']
+	else:
+		server_desc = None
+	return server_desc
+
+
+def get_members_online():
+	"""
+	Gets the number of members currently online
+	:return: The list of members online if any, else None
+	"""
+	server = MinecraftServer.lookup(minecraft_server_ip)
+	status = server.status()
+
+	if 'sample' in [key for key in status.raw['players']]:
+		members_online = [user['name'] for user in status.raw['players']['sample']]
+		return members_online
+	return None
+
+
+async def update_members(interval: int, members_online: get_members_online()):
+	"""
+	Checks for members online in set intervals to update their last seen status
+	:return:
+	"""
+	while True:
+		if members_online is not None:
+			members_file_r = open("./.resources/members.json", "r")
+			members = json.load(members_file_r)
+			members_file_r.close()
+			for i in members_online:
+				if i in members:
+					members[i]["LastSeen"] = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+				else:
+					#  IsAdmin is set to False by default and is updated manually
+					members[i] = {"LastSeen": datetime.now().strftime("%m/%d/%Y %H:%M:%S"), "IsAdmin": False}
+		await asyncio.sleep(interval)
+
+
 def predicate(message, l, r):
+	"""
+
+	:param message: The Discord message being monitored
+	:param l: Checks for a message with a lesser index value in the message list
+	:param r: Checks for a message with a greater index value in the message list
+	:return: The output value of the check function
+	"""
 	def check(reaction, user):
+		"""
+		Checks what reaction to a message the user selected
+		:param reaction: The reaction added by the user
+		:param user: The users id
+		:return: True if the user sent the message and selected a supplied reaction, else False
+		"""
 		if reaction.message.id != message.id or user == client.user:
 			return False
 		if reaction.emoji == emojis['close']:
@@ -60,16 +121,20 @@ def predicate(message, l, r):
 async def on_ready():
 	"""
 	Executed when the client establishes a connection with Discord
+	:return: None
 	"""
-	print('Connection to Discord established succesfully', end='\n')
-	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=listening_to))  # Sets activity status
+	print('Connection to Discord established successfully', end='\n')
+	# Sets activity status
+	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=listening_to))
 	print(f'Client is listening to commands prefixed with {cmd_prefix}', end='\n')
+	await update_members(120, get_members_online())  # checks for status changes in members being online to update json
 
 
 @client.event
 async def on_message(message):
 	"""
-	Checks that the command is called in the minecrafcoords channel
+	Executed when a message is sent in a text channel
+	:return: None
 	"""
 	if message.channel.name == 'general':  # set the name of the channel the bot should listen to here
 		await client.process_commands(message)
@@ -88,12 +153,13 @@ async def find(ctx):
 	pass
 
 
-@client.command(name='random', description='Returns a random POI, if dimmension is specified a random POI in that dimmension is returned')
+@client.command(name='random', description='Returns a random POI, if dimension is specified a random POI in that\
+dimension is returned')
 async def random(ctx):
 	pass
 
 
-@client.command(name='near', description='Returns the five closest POI in the same dimmension as the input coordinates')
+@client.command(name='near', description='Returns the five closest POI in the same dimension as the input coordinates')
 async def near(ctx):
 	pass
 
@@ -105,6 +171,11 @@ async def stats(ctx):
 
 @client.command(name='server', description='Returns server and player info at the time of request')
 async def server(ctx):
+	"""
+	Responds to a call of the server command by a user and supplies server and member statuses
+	:param ctx: Command context passed
+	:return: None
+	"""
 	await ctx.message.delete()  # deletes users message to prevent buildup of commands
 	user_tag = ctx.author.mention
 
@@ -113,14 +184,16 @@ async def server(ctx):
 
 	num_members_online = status.players.online
 	server_latency = status.latency  # in ms
-	# users_connected provides a list of player names currently online
-	if 'sample' in [key for key in status.raw['players']]:
-		members_online = [ user['name'] for user in status.raw['players']['sample'] ]
-	else:
+
+	# get_members_online provides a list of player names currently online
+	if get_members_online() is None:
 		members_online = []
+	else:
+		members_online = get_members_online()
+
 	# server_desc provides the server description, if one exists
-	if 'text' in [key for key in status.raw['description']]:
-		server_desc = status.raw['description']['text']
+	if get_server_description() is not None:
+		server_desc = get_server_description()
 	else:
 		server_desc = ''
 
@@ -152,17 +225,18 @@ async def server(ctx):
 	# messages
 	messages = []
 
-	server_info = discord.Embed(title=f'**{minecraft_server_ip} SERVER INFO**',\
+	server_info = discord.Embed(title=f'**{minecraft_server_ip} SERVER INFO**',
 			description=f"*{server_desc}*", color=0xBA74EE)
-	server_info.set_author(name=client.user.name, icon_url= client.user.avatar_url)
+	server_info.set_author(name=client.user.name, icon_url=client.user.avatar_url)
 	server_info.add_field(name=f"**Server Info**",
 					value=f'***Latency:*** {server_latency}ms\n***Total Members:*** {total_members}\n\
-					***Members online:*** {num_members_online}\n***Admin Online***: {admin_online}\n\n\n\nPage 1/2', inline=True)
+					\r***Members online:*** {num_members_online}\n***Admin Online***: {admin_online}\n\n\n\nPage 1/2',
+						  inline=True)
 	messages.append(server_info)
 
 	member_status = discord.Embed(title=f'**{minecraft_server_ip} MEMBER STATUS**',
 					description=f"*{server_desc}*", color=0xBA74EE)
-	member_status.set_author(name=client.user.name, icon_url= client.user.avatar_url)
+	member_status.set_author(name=client.user.name, icon_url=client.user.avatar_url)
 	member_status.add_field(name='**Member Status**', value=member_list, inline=True)
 	messages.append(member_status)
 
